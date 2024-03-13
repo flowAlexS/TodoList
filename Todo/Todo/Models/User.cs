efb8b43170df49f7d7d28b7182b1c7ee3aca2053
Florescu Alexandru <florescu.alex579774@gmail.com>
2024-03-13 18:13:44 +0200

    Update on profile

--- Todo/Todo/Models/User.cs
+++ Todo/Todo/Models/User.cs
@@ -7,5 +7,15 @@
         public string Username { get; set; }
 
         public string Password { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Phone { get; set; }
+
+        public DateTime? Birthdate { get; set; }
     }
 }
