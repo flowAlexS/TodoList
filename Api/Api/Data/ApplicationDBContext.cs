bca644e791e3e6466d3a6b540b5d5b2b5ff8517a
Florescu Alexandru <florescu.alex579774@gmail.com>
2024-03-13 22:51:51 +0200

    Implemenetd the EF

---
+++ Api/Api/Data/ApplicationDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions dbContextOptions) : base(dbContextOptions) { }
+
+        public DbSet<TodoTask> TodoTasks { get; set; }
+    }
+}
