6a1204f5699f802ca0e5dd44c6265cf83212416c
Florescu Alexandru <florescu.alex579774@gmail.com>
2024-03-13 18:05:50 +0200

    Added userProfile class

---
+++ Todo/Todo/Models/UserProfile.cs
@@ -0,0 +1,15 @@
+﻿namespace Todo.Models
+{
+    public class UserProfile
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public int ProfileId { get; set; }
+
+        public User User { get; set; }
+
+        public Profile Profile { get; set; }
+    }
+}
