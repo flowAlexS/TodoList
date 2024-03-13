ec986aa3b9de3474a6b77912c768e99587e25978
Florescu Alexandru <florescu.alex579774@gmail.com>
2024-03-13 22:47:59 +0200

    refactor TodoTask Model

--- Api/Api/Models/TodoTask.cs
+++ Api/Api/Models/TodoTask.cs
@@ -4,9 +4,9 @@
     {
         public int Id { get; set; }
 
-        public string Title { get; set; }
+        public string Title { get; set; } = string.Empty;
 
-        public string Note { get; set; }
+        public string Note { get; set; } = string.Empty;
 
         public bool Completed { get; set; }
     }
