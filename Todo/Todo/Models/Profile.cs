444a00df33dd8cb8a3e2ba9ac690a63b250f4d9b
Florescu Alexandru <florescu.alex579774@gmail.com>
2024-03-13 18:04:45 +0200

    Initial commit for Models

---
+++ Todo/Todo/Models/Profile.cs
@@ -0,0 +1,17 @@
+﻿namespace Todo.Models
+{
+    public class Profile
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }   
+
+        public string LastName { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public string? Phone { get; set; }
+
+        public string? Email { get; set; }
+    }
+}
