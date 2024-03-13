c0f81bfe82a7c0bc458db0fbe992f304a2daaf31
Florescu Alexandru <florescu.alex579774@gmail.com>
2024-03-13 11:27:19 +0200

    Added ef and models

---
+++ Todo/Todo/Models/Owner.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Todo.Models
+{
+    public class Owner
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string? Email { get; set; }   
+
+        public string? Name { get; set; }
+
+        public string? Password { get; set; }
+    }
+}
