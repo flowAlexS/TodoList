23f2617199cd967eff07a63261249a06c45766f5
Florescu Alexandru <florescu.alex579774@gmail.com>
2024-03-13 18:44:30 +0200

    Implemented the Repository and Repo pattern

---
+++ Todo/Todo/Interfaces/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Todo.Interfaces
+{
+    using Todo.Models;
+
+    public interface IUserRepository
+    {
+        bool Add(User user);
+
+        User GetUser(string username, string password);
+    }
+}
---
+++ Todo/Todo/Repository/UserRepository.cs
@@ -0,0 +1,32 @@
+﻿namespace Todo.Repository
+{
+    using Microsoft.IdentityModel.Tokens;
+    using Todo.Interfaces;
+    using Todo.Models;
+    using Todo.Data;
+
+    public class UserRepository : IUserRepository
+    {
+        private DataContext _context;
+
+        public UserRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public bool Add(User user)
+        {
+            if (this._context.Users.Any(x => x.Username.Equals(user.Username.Trim())))
+            {
+                return false;
+            }
+
+            this._context.Users.Add(user);
+            this._context.SaveChanges();
+            return true;
+        }
+
+        public User? GetUser(string username, string password)
+        => this._context.Users.FirstOrDefault(user => user.Username.Equals(username) && user.Password.Equals(password));
+    }
+}
