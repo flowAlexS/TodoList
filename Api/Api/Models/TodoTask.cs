7da34c19dce32ce1478da712a4400e44a5764b49
Florescu Alexandru <florescu.alex579774@gmail.com>
2024-03-13 22:47:14 +0200

    Added Models

---
+++ Api/Api/Models/TodoTask.cs
@@ -0,0 +1,13 @@
+﻿namespace Api.Models
+{
+    public class TodoTask
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Note { get; set; }
+
+        public bool Completed { get; set; }
+    }
+}
