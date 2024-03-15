38172c00d5a799c1231693d8da3439c17ea42a23
Florescu Alexandru <florescu.alex579774@gmail.com>
2024-03-15 13:08:02 +0200

    Added user model and DbContext

---
+++ TodoApi/TodoApi/Data/ApplicationDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+namespace TodoApi.Data
+{
+    public class ApplicationDBContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDBContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+        }
+    }
+}
